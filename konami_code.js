17f4bfa23868653c0b621c73769c2116c66bd2c0
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-27 18:00:31 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -15,7 +15,7 @@ function init() {
 
   let index = 0
   
-  document.addEventListener('keydown', (e) {
+  document.addEventListener('keydown', (e) => {
     const key = e.key
     
     if (key === codes[index]) {
