07ea6f58af1e78b1f22a4206c6dd0359d7120360
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-27 17:59:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -31,4 +31,21 @@ function init() {
     }
   })
   
+//   function init() {
+//   let idx = 0
+
+//   document.body.addEventListener("keydown", (e) => {
+//     const key = e.key
+    
+//     idx = (codes[idx] === key) ? ++idx : 0
+    
+//     if (idx === codes.length) {
+//       window.alert("Hurray!");
+//       idx = 0
+//     }
+    
+//   });
+// }
+
+  
 }
