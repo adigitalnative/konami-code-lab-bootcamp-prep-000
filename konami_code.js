53a6f38dbc45a52dc3d60dc59c101cd55441dec7
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-27 18:01:26 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -31,21 +31,4 @@ function init() {
     }
   })
   
-//   function init() {
-//   let idx = 0
-
-//   document.body.addEventListener("keydown", (e) => {
-//     const key = e.key
-    
-//     idx = (codes[idx] === key) ? ++idx : 0
-    
-//     if (idx === codes.length) {
-//       window.alert("Hurray!");
-//       idx = 0
-//     }
-    
-//   });
-// }
-
-  
 }
